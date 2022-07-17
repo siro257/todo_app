758d912f6c9a158e4383ba8c6d63f405923e2775
siro257 <siro257@gmail.com>
2022-07-16 23:19:07 -0700

    can edit & complete task

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,35 @@ class MyToDoApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Todo App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return DismissKeyboard(
+      child: MaterialApp(
+        title: 'Todo App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(title: 'Todos'),
       ),
-      home: const MyHomePage(title: 'Todos'),
+    );
+  }
+}
+
+class DismissKeyboard extends StatelessWidget {
+  const DismissKeyboard({Key? key, required this.child}) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+
+        if (!currentFocus.hasPrimaryFocus &&
+            currentFocus.focusedChild != null) {
+          FocusManager.instance.primaryFocus?.unfocus();
+        }
+      },
+      child: child,
     );
   }
 }
@@ -35,13 +58,10 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const TextWidget(),
-          ],
-        ),
+      body: ListView(
+        children: const <Widget>[
+          TextWidget(),
+        ],
       ),
     );
   }
@@ -57,13 +77,20 @@ class TextWidget extends StatefulWidget {
 class _TextWidgetState extends State<TextWidget> {
   List<String> toDoList = [];
   final fieldText = TextEditingController();
-  bool _isCompleted = false;
+  final myFocusNode = FocusNode();
 
-  void _addValue(String value) {
+  @override
+  void dispose() {
+    fieldText.dispose();
+    super.dispose();
+  }
+
+  void _addItem(String value) {
     setState(() {
       toDoList.add(value);
     });
     fieldText.clear();
+    myFocusNode.requestFocus();
   }
 
   void _deleteItem(int idx) {
@@ -72,9 +99,10 @@ class _TextWidgetState extends State<TextWidget> {
     });
   }
 
-  void _toggleTask() {
+  void _editItem(int idx, String newTask) {
     setState(() {
-      _isCompleted = !_isCompleted;
+      toDoList.removeAt(idx);
+      toDoList.insert(idx, newTask);
     });
   }
 
@@ -84,82 +112,165 @@ class _TextWidgetState extends State<TextWidget> {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         Padding(
-          padding: EdgeInsets.symmetric(horizontal: 250, vertical: 16),
+          // padding: const EdgeInsets.symmetric(horizontal: 250, vertical: 16),
+          padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 16),
           child: TextField(
+            keyboardType: TextInputType.text,
             textAlign: TextAlign.center,
             decoration: InputDecoration(
               enabledBorder: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(20),
                 borderSide: const BorderSide(
                   color: Colors.pink,
-                  width: 16.0,
+                  width: 8.0,
                 ),
               ),
               focusedBorder: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(20),
                 borderSide: const BorderSide(
                   color: Colors.blue,
-                  width: 16.0,
+                  width: 8.0,
                 ),
               ),
               hintText: 'Enter your task!',
               filled: true,
               fillColor: Colors.grey[250],
             ),
-            onSubmitted: _addValue,
+            onSubmitted: _addItem,
             controller: fieldText,
+            focusNode: myFocusNode,
           ),
         ),
-        Center(
-          child: Column(
-            children: [
-              // for (var i in toDoList)
-              for (var i = 0; i < toDoList.length; i++)
-                // TODO:
-                // Widgetize this Widget
-                // pass in the index number when creating one
-                // use that index to complete the task/not
-                Padding(
-                  padding: const EdgeInsets.only(top: 8.0),
-                  child: ListTile(
-                    title: InkWell(
-                      onTap: _toggleTask,
-                      child: Text(
-                        toDoList[i],
-                        style: TextStyle(
-                          fontSize: 34,
-                          fontWeight: FontWeight.w300,
-                          letterSpacing: 0.25,
-                          decoration: _isCompleted
-                              ? TextDecoration.lineThrough
-                              : TextDecoration.none,
-                          color: _isCompleted
-                              ? Colors.grey.shade400
-                              : Colors.black,
-                        ),
-                      ),
-                    ),
-                    leading: IconButton(
-                      icon: Icon(Icons.border_color),
-                      onPressed: () => print("EDIT!!!!!!"),
-                      splashRadius: 20.0,
-                    ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.remove),
-                      onPressed: () => _deleteItem(i),
-                      hoverColor: Colors.red.shade100.withOpacity(0.5),
-                      splashRadius: 20.0,
-                    ),
-                    shape: RoundedRectangleBorder(
-                      side: BorderSide(color: Colors.black, width: 1),
-                      borderRadius: BorderRadius.circular(5),
+        Container(
+          padding: const EdgeInsets.only(left: 32),
+          child: Text(
+            toDoList.length <= 1
+                ? "${toDoList.length} task"
+                : "${toDoList.length} tasks",
+            style: Theme.of(context).textTheme.caption,
+          ),
+        ),
+        const Divider(
+          color: Colors.black45,
+        ),
+        for (var i = 0; i < toDoList.length; i++)
+          Todo(
+            id: i,
+            task: toDoList[i],
+            onDeleteTap: _deleteItem,
+            onEditSubmit: _editItem,
+          ),
+      ],
+    );
+  }
+}
+
+class Todo extends StatefulWidget {
+  const Todo(
+      {Key? key,
+      required this.id,
+      required this.task,
+      required this.onDeleteTap,
+      required this.onEditSubmit})
+      : super(key: key);
+  final int id;
+  final String task;
+  final Function(int) onDeleteTap;
+  final Function(int, String) onEditSubmit;
+
+  @override
+  State<Todo> createState() => _TodoState();
+}
+
+class _TodoState extends State<Todo> {
+  bool _isCompleted = false;
+  bool _isEditing = false;
+  final _controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _toggleItem() {
+    setState(() {
+      _isCompleted = !_isCompleted;
+    });
+  }
+
+  void _toggleEdit() {
+    if (!_isEditing) {
+      _controller.text = widget.task;
+    }
+    setState(() {
+      _isEditing = !_isEditing;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8, left: 16, right: 16),
+      child: ListTile(
+        title: InkWell(
+          borderRadius: BorderRadius.all(
+            Radius.circular(5.0),
+          ),
+          onTap: _toggleItem,
+          child: _isEditing
+              ? TextField(
+                  onTap: () => _controller.selection = TextSelection(
+                      baseOffset: _controller.text.length,
+                      extentOffset: _controller.text.length),
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey[250],
+                  ),
+                  onSubmitted: (String val) {
+                    widget.onEditSubmit(widget.id, val);
+                    _toggleEdit();
+                  },
+                  controller: _controller,
+                )
+              : Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Text(
+                    widget.task,
+                    style: TextStyle(
+                      fontSize: 34,
+                      fontWeight: FontWeight.w300,
+                      letterSpacing: 0.25,
+                      decoration: _isCompleted
+                          ? TextDecoration.lineThrough
+                          : TextDecoration.none,
+                      color: _isCompleted ? Colors.grey.shade400 : Colors.black,
                     ),
                   ),
                 ),
-            ],
-          ),
         ),
-      ],
+        leading: IconButton(
+          icon: const Icon(Icons.border_color),
+          onPressed: () => _toggleEdit(),
+          splashRadius: 20.0,
+        ),
+        trailing: IconButton(
+          icon: const Icon(Icons.remove),
+          onPressed: () => widget.onDeleteTap(widget.id),
+          hoverColor: Colors.red.shade100.withOpacity(0.5),
+          splashRadius: 20.0,
+        ),
+        shape: RoundedRectangleBorder(
+          side: const BorderSide(color: Colors.black, width: 1),
+          borderRadius: BorderRadius.circular(5),
+        ),
+      ),
     );
   }
 }
